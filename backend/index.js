f6af73892fa6e8475e237bfd3e6c60ad1f142ffe
andraszyk1 <98710624+andraszyk1@users.noreply.github.com>
2024-01-01 17:21:50 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-//import { db } from './models/db.config.js';
-//import { router as ticketsRouter } from './routes/tickets.router.js';
-//import { router as usersRouter } from './routes/users.router..js';
-//import { router as categoryRouter } from './routes/category.router.js';
-//import { router as loginRouter } from './routes/login.router.js';
-//import { router as acceptticketsRouter } from './routes/accepttickets.router.js';
-//import { router as fileRouter } from './routes/file.router.js';
 
-const app = express();
-const port =  3000;
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to helpdesk application." });
-});
+// index.js
+const express = require('express')
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use('/uploads/', express.static('public/images/'))
+const app = express()
+const PORT = 4000
 
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
 
-//app.use("/users",usersRouter );
-//app.use("/tickets",ticketsRouter );
-//app.use("/accepttickets",acceptticketsRouter );
-//app.use("/categories",categoryRouter );
-//app.use("/",loginRouter );
-//app.use("/file",fileRouter );
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
 
-app.listen(port);
-//await db.sync()
-// await db.sync()
- // .then(() => {
-  //  console.log("Synced db.");
-  //})
-  //.catch((err) => {
-   // console.log("Failed to sync db: " + err.message);
-  //});
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
