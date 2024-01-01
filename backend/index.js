832c395a81700e454bf97b22f26a6b2536f2a6ad
andraszyk1 <98710624+andraszyk1@users.noreply.github.com>
2024-01-01 16:54:12 +0100

    Update index.js
    add comment to check vercel will start without database

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import { db } from './models/db.config.js';
-import { router as ticketsRouter } from './routes/tickets.router.js';
-import { router as usersRouter } from './routes/users.router..js';
-import { router as categoryRouter } from './routes/category.router.js';
-import { router as loginRouter } from './routes/login.router.js';
-import { router as acceptticketsRouter } from './routes/accepttickets.router.js';
-import { router as fileRouter } from './routes/file.router.js';
+//import { db } from './models/db.config.js';
+//import { router as ticketsRouter } from './routes/tickets.router.js';
+//import { router as usersRouter } from './routes/users.router..js';
+//import { router as categoryRouter } from './routes/category.router.js';
+//import { router as loginRouter } from './routes/login.router.js';
+//import { router as acceptticketsRouter } from './routes/accepttickets.router.js';
+//import { router as fileRouter } from './routes/file.router.js';
 
 const app = express();
 const port =  3000;
@@ -20,19 +20,19 @@ app.use(express.urlencoded({extended:true}));
 app.use('/uploads/', express.static('public/images/'))
 
 
-app.use("/users",usersRouter );
-app.use("/tickets",ticketsRouter );
-app.use("/accepttickets",acceptticketsRouter );
-app.use("/categories",categoryRouter );
-app.use("/",loginRouter );
-app.use("/file",fileRouter );
+//app.use("/users",usersRouter );
+//app.use("/tickets",ticketsRouter );
+//app.use("/accepttickets",acceptticketsRouter );
+//app.use("/categories",categoryRouter );
+//app.use("/",loginRouter );
+//app.use("/file",fileRouter );
 
 app.listen(port);
-await db.sync()
+//await db.sync()
 // await db.sync()
-  .then(() => {
-    console.log("Synced db.");
-  })
-  .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
-  });
+ // .then(() => {
+  //  console.log("Synced db.");
+  //})
+  //.catch((err) => {
+   // console.log("Failed to sync db: " + err.message);
+  //});
